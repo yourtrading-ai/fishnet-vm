dec6cd2cbb74a8d996a771396017bcce6815676d
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2023-03-06 12:28:08 +0100

    add discover/deploy api, requirements building/uploading, refactor

--- examples/fishnet_cod/src/fishnet_cod/deployment.py
+++ examples/fishnet_cod/src/fishnet_cod/deployment.py
@@ -1,4 +1,6 @@
 import logging
+import shutil
+import subprocess
 from base64 import b16decode, b32encode
 from pathlib import Path
 from typing import List, Optional
@@ -10,9 +12,9 @@ from semver import VersionInfo
 from aleph.sdk import AuthenticatedAlephClient
 from aleph.sdk.types import StorageEnum
 from aleph.sdk.utils import create_archive
-from aleph_message.models import StoreMessage, MessageType
+from aleph_message.models import StoreMessage, MessageType, ProgramMessage
 
-from .discovery import discover_executors
+from .discovery import discover_executors, discover_apis
 from .constants import FISHNET_DEPLOYMENT_CHANNEL, EXECUTOR_MESSAGE_FILTER, VM_URL_PATH, VM_URL_HOST
 
 from .version import __version__
@@ -28,9 +30,9 @@ async def deploy_executors(
     vcpus: int = 1,
     memory: int = 1024,
     timeout_seconds: int = 900,
-    persistent: bool = True,
-    size_mib: int = 1024 * 10,
-):
+    persistent: bool = False,
+    volume_size_mib: int = 1024 * 10,
+) -> List[ProgramMessage]:
     # Discover existing executor VMs
     executor_messages = await discover_executors(
         deployer_session.account.get_address(), deployer_session, channel
@@ -40,22 +42,7 @@ async def deploy_executors(
     )
 
     # Get latest version executors and source code
-    with deployer_session:
-        source_messages = await deployer_session.get_messages(
-            hashes=source_code_refs, message_type=MessageType.store
-        )
-    latest_source: Optional[StoreMessage] = None
-    for source in source_messages.messages:
-        source: StoreMessage
-        assert (
-            source.content.protocol_version
-        ), "[PANIC] Encountered source_code message with no version!\n" + str(source.json())
-        if not latest_source:
-            latest_source = source
-        elif VersionInfo.parse(source.content.protocol_version) == VersionInfo.parse(
-            latest_source.content.protocol_version
-        ):
-            latest_source = source
+    latest_source = fetch_latest_source(deployer_session, source_code_refs)
     latest_protocol_version = VersionInfo.parse(latest_source.content.protocol_version)
     latest_executors = [
         executor
@@ -79,29 +66,10 @@ async def deploy_executors(
 
     # Upload the source code with new version
     with open(path_object, "rb") as fd:
-        logger.debug("Reading file")
-        # TODO: Read in lazy mode instead of copying everything in memory
-        file_content = fd.read()
-        storage_engine = (
-            StorageEnum.ipfs
-            if len(file_content) > 4 * 1024 * 1024
-            else StorageEnum.storage
-        )
-        logger.debug("Uploading source file")
-        with deployer_session as session:
-            user_code, status = session.create_store(
-                file_content=file_content,
-                storage_engine=storage_engine,
-                channel=FISHNET_DEPLOYMENT_CHANNEL,
-                guess_mime_type=True,
-                extra_fields={
-                    "type": "executor",
-                    "protocol_version": __version__,
-                },
-            )
-        logger.debug("Upload finished")
+        user_code = await upload_source(deployer_session, fd, src_type="executor")
         program_ref = user_code.item_hash
 
+    vm_messages: List[ProgramMessage] = []
     for i, slice_end in enumerate(time_slices[1:]):
         slice_start = time_slices[i - 1]
         # parse slice_end and slice_start to datetime
@@ -117,7 +85,7 @@ async def deploy_executors(
             PersistentVolume(
                 persistence="host",
                 name=name,
-                size_mib=size_mib
+                size_mib=volume_size_mib
             ).dict()
         ]
 
@@ -152,3 +120,146 @@ async def deploy_executors(
             "Visualise on:\n  https://explorer.aleph.im/address/"
             f"{message.chain}/{message.sender}/message/PROGRAM/{hash}\n"
         )
+
+        vm_messages.append(message)
+
+    return vm_messages
+
+
+async def upload_source(deployer_session, fd, src_type):
+    logger.debug("Reading file")
+    # TODO: Read in lazy mode instead of copying everything in memory
+    file_content = fd.read()
+    storage_engine = (
+        StorageEnum.ipfs
+        if len(file_content) > 4 * 1024 * 1024
+        else StorageEnum.storage
+    )
+    logger.debug("Uploading source file")
+    with deployer_session as session:
+        user_code, status = session.create_store(
+            file_content=file_content,
+            storage_engine=storage_engine,
+            channel=FISHNET_DEPLOYMENT_CHANNEL,
+            guess_mime_type=True,
+            extra_fields={
+                "type": src_type,
+                "protocol_version": __version__,
+            },
+        )
+    logger.debug("Upload finished")
+    return user_code
+
+
+async def build_and_upload_requirements(
+    requirements_path: Path,
+    deployer_session: AuthenticatedAlephClient,
+    channel: str = FISHNET_DEPLOYMENT_CHANNEL,
+) -> StoreMessage:
+    logger.debug("Building requirements")
+    opt_packages = Path("/opt/packages")  # /opt/packages is by default imported into Python
+    # check if directory exists, clean if necessary
+    if not opt_packages.exists():
+        opt_packages.mkdir()
+    else:
+        shutil.rmtree(opt_packages)
+        opt_packages.mkdir()
+    # install requirements
+    subprocess.run(
+        ["pip", "install", "-t", str(opt_packages), "-r", str(requirements_path)],
+        check=True,
+    )
+    # build file system image
+    squashfs_path = requirements_path.parent / "packages.squashfs"
+    subprocess.run(
+        ["mksquashfs", str(opt_packages), str(squashfs_path)],
+        check=True,
+    )
+    # remove temporary directory
+    shutil.rmtree(opt_packages)
+    # upload requirements
+    with open(squashfs_path, "rb") as fd:
+        logger.debug("Uploading requirements")
+        with deployer_session as session:
+            user_code, status = session.create_store(
+                file_content=fd.read(),
+                storage_engine=StorageEnum.ipfs,
+                channel=channel,
+                guess_mime_type=True,
+                extra_fields={
+                    "mount_point": opt_packages,
+                    "type": "requirements",
+                    "protocol_version": __version__,
+                },
+            )
+        logger.debug("Upload finished")
+        return user_code
+
+
+def deploy_api(
+    api_path: Path,
+    deployer_session: AuthenticatedAlephClient,
+    executors: List[ProgramMessage],
+    channel: str = FISHNET_DEPLOYMENT_CHANNEL,
+    vcpus: int = 1,
+    memory: int = 1024 * 4,
+    timeout_seconds: int = 900,
+    persistent: bool = False,
+) -> ProgramMessage:
+    # Discover existing executor VMs
+    api_messages = await discover_apis(
+        deployer_session.account.get_address(), deployer_session, channel
+    )
+    source_code_refs = set([api.content.code.ref for api in api_messages])
+
+    latest_source = fetch_latest_source(deployer_session, source_code_refs)
+    latest_protocol_version = VersionInfo.parse(latest_source.content.protocol_version)
+    latest_apis = [
+        api
+        for api in api_messages
+        if api.content.code.ref == latest_source.item_hash
+    ]
+
+    # Create new source archive from local files and hash it
+    path_object, encoding = create_archive(api_path)
+
+    # Check versions of latest source code and latest apis
+    if latest_protocol_version >= __version__:
+        raise Exception(
+            "Latest protocol version is equal or greater than current version, aborting deployment: "
+            + f"({latest_protocol_version} >= {__version__})"
+        )
+    version_string = f"v{__version__.major}.{__version__.minor}.{__version__.patch}-{__version__.prerelease}"
+
+    # Upload the source code with new version
+    with open(path_object, "rb") as fd:
+        user_code = await upload_source(deployer_session, fd, src_type="api")
+        program_ref = user_code.item_hash
+
+    # Create immutable volume with python dependencies
+    volumes = [
+        ImmutableVolume(ref=program_ref).dict(),  # TODO: Get ref from dependencies image
+    ]
+
+    # Register the program
+
+
+def fetch_latest_source(deployer_session, source_code_refs):
+    # Get latest version executors and source code
+    with deployer_session:
+        source_messages = await deployer_session.get_messages(
+            hashes=source_code_refs, message_type=MessageType.store
+        )
+    latest_source: Optional[StoreMessage] = None
+    for source in source_messages.messages:
+        source: StoreMessage
+        assert (
+            source.content.protocol_version
+        ), "[PANIC] Encountered source_code message with no version!\n" + str(source.json())
+        if not latest_source:
+            latest_source = source
+        elif VersionInfo.parse(source.content.protocol_version) == VersionInfo.parse(
+                latest_source.content.protocol_version
+        ):
+            latest_source = source
+    return latest_source
